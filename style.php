f16ef97f63d7fd16132724fca09f0f38fab43d26
bimafm17 <72851360+bimafm17@users.noreply.github.com>
2020-10-14 13:28:36 +0700

    Update style.php

--- style.php
+++ style.php
@@ -7,8 +7,6 @@
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 		<script src="jquery/jquery-3.5.1.min.js"></script>
 		<script src="popper/popper.min.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
+        	<script src="bootstrap/js/bootstrap.min.js"></script>
 	</head>
-	<body>
-	</body>
 </html>
